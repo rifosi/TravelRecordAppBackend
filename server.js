1190c0d492bb5a6b09edb84e914623a87b12d7bd
Glareone <enlarius@gmail.com>
2019-12-01 21:32:02 +0300

    added Delivery table for Delivery Application (because of backend for deliveryApp doesn't work properly)

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('Post'); // Create a table for 'Post' with default settings
-mobileApp.tables.add('Users'); // Create a table for 'Users' (Plural, because user is reserved word in SQL)
+mobileApp.tables.add('Post'); // Add a table for 'Post' with default settings
+mobileApp.tables.add('Users'); // Add a table for 'Users' (Plural, because user is reserved word in SQL)
+mobileApp.tables.add('Delivery'); // Add a table for 'Delivery' with default settings
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
